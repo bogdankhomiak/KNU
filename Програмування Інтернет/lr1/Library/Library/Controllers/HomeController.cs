e643f2c83161c9de8026eec18bdd60e58a5b6915
bogdan <peculiarsev@gmail.com>
2017-10-26 14:37:47 +0300

    init

---
+++ Програмування Інтернет/lr1/Library/Library/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class HomeController : Controller
+        {
+            // GET: /Home/
+            BookContext db = new BookContext();
+            public ActionResult Index()
+            {
+            // Отримуємо з БД всі об'єкти Book
+                IEnumerable<Book> books = db.Books;
+            // Передаємо всі  об'єкти в динамічну властивість Books в ViewBag
+                ViewBag.Books = books;
+            // Повертаємо представлення
+                return View();
+            }
+            [HttpGet]
+            public ActionResult Buy(int id)
+            {
+                ViewBag.BookId = id;
+                return View();
+            }
+            [HttpPost]
+            public string Buy(Purchase purchase)
+            {
+                purchase.Date = DateTime.Now;
+                // Додаємо інформацію про покупку в базу даних
+                db.Purchases.Add(purchase);
+                //  Зберігаємо в БД всі зміни
+                db.SaveChanges();
+                return "Спасибі," + purchase.Person + ", за покупку!";
+            }
+        }
+}
---
+++ Програмування Інтернет/lr1/Library/Library/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        // ID книги
+        public int Id { get; set; }
+        // назва книги
+        public string Name { get; set; }
+        // автор книги
+        public string Author { get; set; }
+        // ціна
+        public int Price { get; set; }
+    }
+    public class Purchase
+    {
+        // ID покупки
+        public int PurchaseId { get; set; }
+        // ім’я і прізвище покупця
+        public string Person { get; set; }
+        // адреса покупця
+        public string Address { get; set; }
+        // ID книги
+        public int BookId { get; set; }
+        // дата покупки
+        public DateTime Date { get; set; }
+    }
+
+}

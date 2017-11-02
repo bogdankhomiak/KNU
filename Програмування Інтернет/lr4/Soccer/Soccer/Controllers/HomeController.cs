263316ff4249dc48fbb74f5aeb9b98394ea4bab6
bogdan <peculiarsev@gmail.com>
2017-11-02 22:35:48 +0200

    init

---
+++ Програмування Інтернет/lr4/Soccer/Soccer/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using Soccer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Soccer.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        SoccerContext db = new SoccerContext();
+        // Виводимо всіх футболістів
+        public ActionResult Index()
+        {
+            var players = db.Players.Include(p => p.Team);
+            return View(players.ToList());
+        }
+        public ActionResult TeamDetails(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            team.Players = db.Players.Where(m => m.TeamId == team.Id);
+            return View(team);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //Формуємо список команд для передачі в представлення
+            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+            ViewBag.Teams = teams;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            // Додаємо гравця в таблицю
+            db.Players.Add(player);
+            db.SaveChanges();
+            // перенаправляємо на головну сторінку
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            // Знаходимо в БД футболіста
+            Player player = db.Players.Find(id);
+            if (player != null)
+            {
+                // Створюємо список команд для передачі в представлення
+                SelectList teams = new SelectList(db.Teams, "Id", "Name", player.TeamId);
+                ViewBag.Teams = teams;
+                return View(player);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Player player)
+        {
+            db.Entry(player).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+
+}

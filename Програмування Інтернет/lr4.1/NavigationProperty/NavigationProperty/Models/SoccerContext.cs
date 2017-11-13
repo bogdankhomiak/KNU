b3ff092889d92061ca071c95af4ab65d03379541
bogdan <peculiarsev@gmail.com>
2017-11-13 12:39:35 +0200

    init

---
+++ Програмування Інтернет/lr4.1/NavigationProperty/NavigationProperty/Models/SoccerContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NavigationProperty.Models
+{
+    public class SoccerContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+
+}
